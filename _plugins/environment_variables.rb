88ad1700aaa31423d3889523aba3d2403a9b4e55
Dani Roxberry <dani@bitoriented.com>
2017-12-22 13:50:09 -0800

    [wip] experiments

--- _plugins/environment_variables.rb
+++ _plugins/environment_variables.rb
@@ -6,7 +6,7 @@ module Jekyll
     def generate(site)
       # NOTE: Assigning to ENV will not work in liquid templates
       # must iterate through the keys & values and build a new map
-      site.config['env'] = Dotenv.load
+      site.config['env'] = Dotenv.load('.env')
     end
   end
 end
