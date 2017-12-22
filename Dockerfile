19ec1bbbb825f3792b50c727f3124dbe56559a7e
Dani Roxberry <dani@bitoriented.com>
2017-12-22 10:38:25 -0800

    [wip] almost have env plugin working

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,2 @@
 FROM starefossen/github-pages:onbuild
+RUN ruby -S gem install dotenv
--- _plugins/environment_variables.rb
+++ _plugins/environment_variables.rb
@@ -6,7 +6,9 @@ module Jekyll
     priority :highest
 
     def generate(site)
-      site.config['env'] = Dotenv.load
+      site.config['env'] = ENV['JEKYLL_ENV'] || 'development'
+      # Add other environment variables to `site.config` here...
+      Jekyll.logger.debug site.config.to_yaml
     end
   end
 end
